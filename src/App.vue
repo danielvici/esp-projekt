4dc0f653adfc24de3b23e334a27fe237985f464b
Esad Mustafoski <Lynixenn>
2024-11-01 20:49:50 +0100

    Comments

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-import Sidebar from "./components/home_components/sidebar.vue";
-import NiceThings from "./components/home_components/interesting-hashtags.vue";
-import Feed from "./components/home_components/feed.vue";
-import Contacts from "./components/home_components/contacts.vue";
-import Legal from "./components/home_components/legal.vue";
-</script>
-
-
 <template>
   <router-view />
 </template>
