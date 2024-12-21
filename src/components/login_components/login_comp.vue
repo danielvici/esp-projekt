1d8665d6ce6733d0ead72273b3bccbd48831d9c6
danielvici123 <94993276+danielvici@users.noreply.github.com>
2024-12-21 11:12:43 +0100

    small changes+ dokcer file added (NOT WORKING)
    small changes
    dockerfile added but its not working

--- src/components/login_components/login_comp.vue
+++ src/components/login_components/login_comp.vue
@@ -46,14 +46,14 @@ function handleSubmit(event: Event) {
 <template>
   <div class="px-20 border-x border-x-grau2 pb-35p">
     <div class="text-3xl pt-32"> <!-- ÜBERSCHRIFT-->
-      <p class="text-weiss text-center">Welcome to <span class="text-button-farbe">ESP</span>!</p>
+      <p class="text-weiss text-center">Welcome to <span class="text-logo-farbe-rot">E</span><span class="text-logo-farbe-lila">S</span><span class="text-logo-farbe-blau">P</span>!</p>
       <p class="text-weiss text-center">Login or create a new Account to continue</p>
     </div>
     <div class="px-20 pt-7"><!--  FORM --->
       <form class="flex flex-col items-center" @submit.prevent="handleSubmit">
         <input class="m-4 w-full max-w-xs bg-grau-dunkel p-4 text-weiss placeholder-grau2 focus:outline-none rounded-lg" v-model="input_username_mail" type="text" placeholder="Username or E-Mail"><br>
         <input class="m-4 w-full max-w-xs bg-grau-dunkel p-4 text-weiss placeholder-grau2 focus:outline-none rounded-lg" v-model="input_user_password" type="password" placeholder="Password"><br>
-        <button class="m-4 bg-grau-dunkel w-full max-w-xs p-4 text-weiss rounded-lg" @click="login()">Login</button>
+        <button class="m-4 bg-button-farbe w-full max-w-xs p-4 text-grau-dunkel rounded-lg" @click="login()">Login</button>
       </form>
     </div>
   </div>
